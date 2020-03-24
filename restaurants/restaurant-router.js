7aeed5e61fceab279fe6706631e722362beeced8
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2020-03-24 08:43:27 -0400

    added new enf points for all categoties

--- restaurants/restaurant-router.js
+++ restaurants/restaurant-router.js
@@ -38,10 +38,192 @@ router.get('/events/:latitude/:longitude', async (req, res) => {
         })
 })
 
-router.get('/categories/:latitude/:longitude/:categorie', async (req, res) => {
+router.get('/categories/musicvenues/:latitude/:longitude', async (req, res) => {
     const info = await req.params
 
-    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=${info.categorie}`, config)
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=musicvenues`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/coffee/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=coffee`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/tours/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=tours`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/museums/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=museums`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/theater/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=theater`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/sportsteams/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=sportsteams`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/parks/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=parks`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/active/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=active`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/social_clubs/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=social_clubs`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/fashion/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=fashion`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/furniture/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=furniture`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/hardware/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=hardware`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/hotels/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=hotels`, config)
+        .then(response => {
+            // console.log(response.data)
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.get('/categories/publicservicesgovt/:latitude/:longitude', async (req, res) => {
+    const info = await req.params
+
+    axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${info.latitude}&longitude=${info.longitude}&limit=20&categories=publicservicesgovt`, config)
         .then(response => {
             // console.log(response.data)
             res.status(200).json(response.data)
